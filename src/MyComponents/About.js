d61850f645a9ce2047f543ce5280ffe108c39959
Yash15-maker <1905073@kiit.ac.in>
2021-05-15 18:43:44 +0530

    initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div  style={{minHeight: "80vh"}}>
+            <h3 style={{color:`#d2691e`}}><i>About Todo App</i></h3> 
+            <p style={{color:"beige"}}><i>This is a todo app to be used personally.
+                <br/>This by no means is a secure note posting website as anyone can add or delete any note.<br/>The Login and Signup feature is to be added later.</i></p></div>
+    )
+}
+
+
+/*<h3 style={{color:"pink"}}><i>About Todo App</i></h3> 
+            <p style={{color:"beige"}}><i>This is a todo app to be used personally.
+                <br/>This by no means is a secure note posting website as anyone can add or delete any note.<br/>The Login and Signup feature is to be added later.</i></p>*/
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+
+        <footer className="bg-success text-dark py-3" >
+            <p className="text-center" >
+            <b>Copyright &copy; Yash Mishra</b>
+            </p>
+        </footer>
+    )
+}
