563819a9c99356412f709604e5871de56325ea1a
Yash15-maker <64361746+Yash15-maker@users.noreply.github.com>
2021-05-16 11:34:03 +0530

    Update Todos.js

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -5,7 +5,7 @@ export const Todos = (props) => {
     return (
         <div className="container" style={{minHeight: "40vh", margin: "1rem auto"}}>
             <h3 className=" my-3">Todos List</h3>
-            {props.todos.length===0?"No todos to display. Jaake bnao phir aao :) ":
+            {props.todos.length===0?"No todos to display :( ":
             props.todos.map((todo) => 
             { return <TodoItem todo={todo} key={todo.sno}
             onDelete={props.onDelete} /> })}
